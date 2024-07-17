6d3dbc05d73000e8d25658a6761857989c9d6fa4
Patrick O'Neill <patrick@rivosinc.com>
2024-07-17 11:13:00 -0700

    Add basic fuzzer compiler flag generator

---
+++ scripts/random-compiler-flags-to-fuzz.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+for i in $(seq 1 $1); do
+
+case $(( RANDOM % 3 )) in
+0) printf -- "-march=rv64gcv" ;;
+1) printf -- "-march=rv64gcv_zvl256b" ;;
+2) printf -- "-march=rv64gcv_zvl512b" ;;
+2) printf -- "-march=rv64gcv_zvl1024b" ;;
+esac
+printf -- " "
+
+case $(( RANDOM % 2 )) in
+0) printf -- "" ;;
+1) printf -- "-mtune=generic-ooo" ;;
+esac
+printf -- " "
+
+case $(( RANDOM % 2 )) in
+0) printf -- "" ;;
+1) printf -- "-flto" ;;
+esac
+printf -- " "
+
+case $(( RANDOM % 2 )) in
+0) printf -- "" ;;
+1) printf -- "-mrvv-vector-bits=zvl" ;;
+esac
+printf -- " "
+
+case $(( RANDOM % 5 )) in
+0) printf -- "" ;;
+1) printf -- "-mrvv-max-lmul=dynamic" ;;
+2) printf -- "-mrvv-max-lmul=m1" ;;
+3) printf -- "-mrvv-max-lmul=m2" ;;
+4) printf -- "-mrvv-max-lmul=m8" ;;
+esac
+printf -- " "
+
+printf -- "-O3 "
+
+printf "\n"
+
+done
